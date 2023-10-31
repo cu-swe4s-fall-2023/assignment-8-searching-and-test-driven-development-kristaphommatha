e788c8e13383b4ecf9e14c42a72ebe313a9b8b8f
Krista <krph6495@colorado.edu>
2023-10-31 21:49:29 +0000

    add a test data file

--- test/unit/test_fire_gdp.py
+++ test/unit/test_fire_gdp.py
@@ -15,7 +15,7 @@ class TestFireGDP(unittest.TestCase):
 
     def test_get_data_bad_index(self):
         bad_index_code = -2
-        self.assertEqual(fire_gdp.get_data('../../data/Agrofood_co2_emission.csv', 10000, 'query value'), bad_index_code)
+        self.assertEqual(fire_gdp.get_data('../data/test_data.csv', 10000, 'query value'), bad_index_code)
 
 if __name__ == "__main__":
     unittest.main()
