8968822b5b5951af2959bed3c129da609117699d
Krista <krph6495@colorado.edu>
2023-10-31 22:39:26 +0000

    add positive no header test

--- test/unit/test_fire_gdp.py
+++ test/unit/test_fire_gdp.py
@@ -8,14 +8,29 @@ import os
 
 
 class TestFireGDP(unittest.TestCase):
-    
+
     def test_get_data_no_file(self):
         no_file_code = -1
-        self.assertEqual(fire_gdp.get_data('NoFile.txt', 0, 'query value'), no_file_code)
+        self.assertEqual(fire_gdp.get_data('NoFile.txt', 0, 'query value'),
+                         no_file_code)
 
     def test_get_data_bad_index(self):
         bad_index_code = -2
-        self.assertEqual(fire_gdp.get_data('../data/test_data.csv', 10000, 'query value'), bad_index_code)
+        self.assertEqual(fire_gdp.get_data('../data/test_data.csv', 10000,
+                                           'query value'),
+                         bad_index_code)
+
+    def test_get_data_positive_no_header(self):
+        test_query_col = 0
+        test_query_value = "Query"
+        func_return = fire_gdp.get_data('../data/test_data.csv',
+                                        test_query_col, test_query_value,
+                                        header=False)
+        expected = [['Query', '1990', '14.7237', '0.0557', '205.6077', '0'],
+                    ['Query', '1991', '14.7237', '0.0557', '0', '0'],
+                    ['Query', '1992', '14.7237', '0.0557', '196.5341', '0']]
+        self.assertListEqual(func_return, expected_return)
+
 
 if __name__ == "__main__":
     unittest.main()
