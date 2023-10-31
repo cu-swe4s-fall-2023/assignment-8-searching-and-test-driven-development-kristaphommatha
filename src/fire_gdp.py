87935b2286fea0cfc70e8a5e4f44ce89d9f061cb
Krista <krph6495@colorado.edu>
2023-10-31 22:57:14 +0000

    fix function

--- src/fire_gdp.py
+++ src/fire_gdp.py
@@ -1,14 +1,13 @@
-def get_data(file_name, query_column, query_value, header = True):
+def get_data(file_name, query_column, query_value, header=True):
     result_row = []
-    header = []
     is_on_header = True
     try:
         with open(file_name, 'r') as file:
             for line in file:
                 data = line.rstrip().split(',')
-                if header == True and is_on_header:
-                # if header is true, let header be the list in the first index
-                    result_row.append(data)
+                if header is True:
+                    if is_on_header is True:
+                        result_row.append(data)
                 if data[query_column] == str(query_value):
                     result_row.append(data)
                 is_on_header = False
@@ -19,8 +18,10 @@ def get_data(file_name, query_column, query_value, header = True):
 
     return result_row
 
+
 def search():
     pass
-    
+
+
 def get_fire_gdp_year_data():
     pass
