5ead7e57ff7a7c78d797bb49d1bbbdafc85fe748
Krista <krph6495@colorado.edu>
2023-11-01 01:19:33 +0000

    add splitter func to split gdp file

--- src/fire_gdp.py
+++ src/fire_gdp.py
@@ -30,5 +30,44 @@ def search(nums, key):
     return indexes
 
 
+def splitter(string, delimiter1, delimiter2):
+    new_strings = []
+    delim2_start_index = -1
+    # flag to indicate that we are in between two delimiter1s
+    delim1_bool = False
+    char_counter = 0
+    for char in string:
+        # 1st quotation was found
+        if (char == delimiter1 and delim1_bool is False):
+            delim1_bool = True
+            delim1_start_index = char_counter  # save location of 1st delim1
+
+        # 2nd quotation was found
+        elif (char == delimiter1 and delim1_bool is True):
+            new = string[delim1_start_index + 1:char_counter]
+            new_strings.append(new)
+            delim1_bool = False
+            delim2_start_index = char_counter + 1
+
+        # Internal comma was found
+        if (char == delimiter2 and delim1_bool is True):
+            pass
+
+        # External comma was found
+        if (char == delimiter2 and delim1_bool is False):
+            # catch that no empty strings are made
+            if delim2_start_index != char_counter:
+                new = string[delim2_start_index + 1:char_counter]
+                delim2_start_index = char_counter  # Update location of delim2
+                new_strings.append(new)
+        if char_counter == len(string) - 1:  # at the end of the string
+            if char != '"':
+                new = string[delim2_start_index + 1:char_counter + 1]
+                new_strings.append(new)
+        char_counter += 1
+
+    return new_strings
+
+
 def get_fire_gdp_year_data():
     pass
