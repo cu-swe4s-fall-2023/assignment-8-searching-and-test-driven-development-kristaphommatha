102b44c0c4f1d9adeed1ac48a2c7672c744d46d0
Krista <krph6495@colorado.edu>
2023-11-06 07:29:30 +0000

    add python file to call data cleaning func

---
+++ data/cleaning_data.py
@@ -0,0 +1,26 @@
+import sys
+sys.path.insert(0, '../UsingLibraries')  # noqa
+import dataFunctions as dfs
+import argparse
+
+
+def main():
+    try:
+        parser = argparse.ArgumentParser(description='Clean data file',
+                                         prog='dataFunctions.py')
+
+        parser.add_argument('--f',
+                            type=str,
+                            help='Name of data file to clean',
+                            required=True)
+        args = parser.parse_args()
+        
+    except Exception as e:
+        sys.exit(1)
+    
+    dfs.clean_data(args.f)
+    print('Cleanup completed')
+
+
+if __name__ == "__main__":
+    main()
