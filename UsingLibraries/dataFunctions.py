5cfc02437a8db5c97c217a4c2e1f01957e3009a3
Krista <krph6495@colorado.edu>
2023-11-06 07:31:15 +0000

    Minor fixes

--- UsingLibraries/dataFunctions.py
+++ UsingLibraries/dataFunctions.py
@@ -50,7 +50,7 @@ def get_fire_gdp_year_data(fires_file, gdp_file, country, target_stats):
         return None
 
     country_fires_df = collect_fire_years_data(fires_file, 'Area',
-                                               country, target_stat)
+                                               country, target_stats)
 
     if country_fires_df != -1 or country_fires_df != 0:
         country_gdp_df = gdp_df[gdp_df['Country'] == country]
