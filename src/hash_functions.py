a83cf2621f56cc16e215fe54bb6d638ff7b65257
Krista <krph6495@colorado.edu>
2023-11-27 02:33:46 +0000

    modify insert to take in existing list

--- src/hash_functions.py
+++ src/hash_functions.py
@@ -11,13 +11,13 @@ def get_index(key, N, p=53, m=2**64):
     return s % N
 
 
-def h_insert(key, value, N):
+def h_insert(key, value, N, table=None):
     if N == 0:
         return None
     if type(key) != str:  # noqa
         return None
-
-    table = [[] for i in range(N)]
+    if table is None:
+        table = [[] for i in range(N)]
     hash_slot = get_index(key, N)
     table[hash_slot].append((key, value))
     return table
